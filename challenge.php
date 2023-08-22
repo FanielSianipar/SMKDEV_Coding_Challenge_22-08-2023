62758d85c60872a444131cdf2005dde76558dbba
FanielSianipar <fanielsianipar01@gmail.com>
2023-08-22 17:29:00 +0700

    revisi

--- challenge.php
+++ challenge.php
@@ -1,12 +1,7 @@
 <?php
 // Coding Challenge SMKDEV 22 Agustus 2023
 
-$variabel = 0;
-
-echo "Masukkan nilai variabel = ";
-$variabel = readline();
-
-echo "\nNilai variabel = " . $variabel . "\n";
+$variabel = 5;
 
 for ($i = $variabel; $i >= 1; $i--) {
     for ($j = $i; $j >= 1; $j--) {
